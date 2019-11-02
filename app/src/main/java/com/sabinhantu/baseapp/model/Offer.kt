4443ccb8d8ec114fcd0f3fe620b9572e3b052382
Sabin Hantu <sabinhantu@gmail.com>
2019-11-02 22:01:31 +0200

    add offer donation model

---
+++ app/src/main/java/com/sabinhantu/baseapp/model/Offer.kt
@@ -0,0 +1,34 @@
+package com.sabinhantu.baseapp.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class Offer (
+    @SerializedName("id")
+    @Expose
+    var id: Long = 0,
+
+    @SerializedName("companyId")
+    @Expose
+    var companyId: Long,
+
+    @SerializedName("date")
+    @Expose
+    var date: String,
+
+    @SerializedName("quantity")
+    @Expose
+    var quantity: Int,
+
+    @SerializedName("reserved")
+    @Expose
+    var reserved: Int,
+
+    @SerializedName("inHouse")
+    @Expose
+    var inHouse: Boolean,
+
+    @SerializedName("description")
+    @Expose
+    var description: String
+)
