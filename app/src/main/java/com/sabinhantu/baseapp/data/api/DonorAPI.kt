b2e90ab5d79c6e5c74e64560947b7ba54f4e4962
Bogdan Noana <nbogdand@yahoo.com>
2019-11-03 03:09:20 +0200

    add request for get food requests

--- app/src/main/java/com/sabinhantu/baseapp/data/api/DonorAPI.kt
+++ app/src/main/java/com/sabinhantu/baseapp/data/api/DonorAPI.kt
@@ -3,14 +3,20 @@ package com.sabinhantu.baseapp.data.api
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import com.sabinhantu.baseapp.model.Offer
+import com.sabinhantu.baseapp.model.RequestFood
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface DonorAPI {
 
     @POST("donor/offer")
     fun createOffer(@Body offer: OfferRequest): Call<Offer>
+
+    @GET("donor/show-requests")
+    fun getRequests(): Call<ArrayList<RequestFood>>
 }
 
 data class OfferRequest (
